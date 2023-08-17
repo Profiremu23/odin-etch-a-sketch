3ff07dc31a55f84efed258455552d0aec0198594
Profiremu23 <Profirdani23@gmail.com>
2023-08-17 18:27:00 +0200

    Fixed hovering bug

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const container = document.getElementById('container');
 function createTable(cells) {
     for (let i = 0; i < cells; i++) {
         let cell = document.createElement('div');
+        cell.classList.add('frame');
         container.appendChild(cell);
     }
 }
 createTable(256);
+
+// Hovering to color
+const frames = document.querySelectorAll('.frame');
+frames.forEach((div) => div.addEventListener('mouseover', () => {
+    div.style.backgroundColor = "#000000";
+}));
